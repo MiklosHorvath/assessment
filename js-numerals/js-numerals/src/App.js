0ad5fb2185f44caa14297e5f2f848fa376bba9ed
MiklosHorvath <horvathmikloswork@gmail.com>
2020-09-15 23:20:56 +0200

    implements solution from 0 to 19

--- js-numerals/js-numerals/src/App.js
+++ js-numerals/js-numerals/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
+  const [solution, setSolution] = useState("");
+  const [numInput, setNumInput] = useState(0);
+
+  const handleInputNumberChange = (e) => {
+    setNumInput(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    var numbersFromZeroToNineTeen = [
+      "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+      "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    setSolution(numbersFromZeroToNineTeen[numInput]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input data-testid="numeric-input" id="number" type="number" onChange={handleInputNumberChange}></input>
+      <Button data-testid="submit-button" type="primary" onClick={handleSubmit}>Convert to english text</Button>
+      <br/>
+      <b>in english</b><div data-testid="solution">{solution}</div>
     </div>
+
   );
 }
 
--- js-numerals/js-numerals/src/App.test.js
+++ js-numerals/js-numerals/src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders corectly', () => {
+  const {queryByTestId} = render(<App/>)
+  expect(queryByTestId("numeric-input")).toBeTruthy();
+  expect(queryByTestId("submit-button")).toBeTruthy();
 });
+
+it("can solve 0",() =>{
+  const {queryByTestId} = render(<App/>)
+  const soluton = queryByTestId("solution");
+  const numInput = queryByTestId("numeric-input");
+  const submitButton = queryByTestId("submit-button");
+  fireEvent.change(numInput, {target: {value: "0"}});
+  fireEvent.click(submitButton);
+  expect(soluton.textContent).toEqual("zero");
+})
+
+it("can solve 11",() =>{
+  const {queryByTestId} = render(<App/>)
+  const soluton = queryByTestId("solution");
+  const numInput = queryByTestId("numeric-input");
+  const submitButton = queryByTestId("submit-button");
+  fireEvent.change(numInput, {target: {value: "11"}});
+  fireEvent.click(submitButton);
+  expect(soluton.textContent).toEqual("eleven");
+})
+
+it("can solve 19",() =>{
+  const {queryByTestId} = render(<App/>)
+  const soluton = queryByTestId("solution");
+  const numInput = queryByTestId("numeric-input");
+  const submitButton = queryByTestId("submit-button");
+  fireEvent.change(numInput, {target: {value: "19"}});
+  fireEvent.click(submitButton);
+  expect(soluton.textContent).toEqual("nineteen");
+})
